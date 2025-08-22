68cf25fde31041d37acafc061a2e4a39fb4038e9
gaurav-11-opp <kuntalgaurav11@gmail.com>
2025-08-22 23:07:39 +0530

    question3.cpp

---
+++ Level2/question3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double perimeter;
+    cout << "Enter perimeter of square: ";
+    cin >> perimeter;
+    double side = perimeter / 4;
+    cout << "The length of the side is " << side
+         << " whose perimeter is " << perimeter << endl;
+
+    return 0;
+}
