6cade9700b1a31b2d5e7c520ea01a4c4fa71b3b2
gaurav-11-opp <kuntalgaurav11@gmail.com>
2025-08-22 22:32:06 +0530

    question3.cpp

---
+++ week1/question3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double kilometers = 10.8;
+    double miles = kilometers * 1.6;
+
+    cout << "The distance " << kilometers << " km in miles is " << miles << endl;
+
+    return 0;
+}
