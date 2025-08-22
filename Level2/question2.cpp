5a0b4511029796bf059873c9c043fb6cb3c7a6fe
gaurav-11-opp <kuntalgaurav11@gmail.com>
2025-08-22 23:07:00 +0530

    question2.cpp

---
+++ Level2/question2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double base, height;
+    cout << "Enter base (cm) and height (cm): ";
+    cin >> base >> height;
+    double area_cm = 0.5 * base * height;
+    double area_in = area_cm / (2.54 * 2.54);
+    cout << "The Area of the triangle in sq in is " << area_in
+         << " and sq cm is " << area_cm << endl;
+
+    return 0;
+}
