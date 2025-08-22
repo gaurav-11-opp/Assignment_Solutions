c7d20e2bb9a00dbaf1e18eef301426931fb66279
gaurav-11-opp <kuntalgaurav11@gmail.com>
2025-08-22 22:28:44 +0530

    question1.cpp

---
+++ week1/question1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int birthYear=2000;
+    int currentYear=2024;
+    int age=currentYear - birthYear;
+
+    cout <<"Harry's age in"<< currentYear << " is " <<age<<endl;
+
+    return 0;
+}
