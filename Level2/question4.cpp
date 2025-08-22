d7f9bb1a34493345726cb1d95785ae29a9d6f8bf
gaurav-11-opp <kuntalgaurav11@gmail.com>
2025-08-22 23:08:11 +0530

    question4.cpp

---
+++ Level2/question4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double feet;
+    cout << "Enter distance in feet: ";
+    cin >> feet;
+    double yards = feet / 3;
+    double miles = yards / 1760;
+    cout << "The distance in yards is " << yards
+         << " while the distance in miles is " << miles << endl;
+
+    return 0;
+}
