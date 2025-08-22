d36f51e03997993536558c0011afb46764ef0ffa
gaurav-11-opp <kuntalgaurav11@gmail.com>
2025-08-22 22:31:11 +0530

    question2.cpp

---
+++ week1/question2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int maths = 94;
+    int physics = 95;
+    int chemistry = 96;
+
+    float average = (maths + physics + chemistry) / 3.0;
+
+    cout << "Sam's average mark in PCM is " << average << endl;
+
+    return 0;
+}
