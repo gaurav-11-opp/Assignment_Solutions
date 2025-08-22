a45b8db9b12a3aa43589cbbd373d56387b554ce4
gaurav-11-opp <kuntalgaurav11@gmail.com>
2025-08-22 23:08:45 +0530

    question5.cpp

---
+++ Level2/question5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double unitPrice;
+    int quantity;
+    cout << "Enter unit price and quantity: ";
+    cin >> unitPrice >> quantity;
+    double total = unitPrice * quantity;
+    cout << "The total purchase price is INR " << total
+         << " if the quantity " << quantity
+         << " and unit price is INR " << unitPrice << endl;
+
+    return 0;
+}
