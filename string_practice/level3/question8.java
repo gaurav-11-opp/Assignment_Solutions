3e6b53caffbb8f71212d23e9c6c2eddbf48428db
gaurav-11-opp <kuntalgaurav11@gmail.com>
2025-08-30 23:44:49 +0530

    question8.java

---
+++ string_practice/level3/question8.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class AnagramChecker {
+
+    public static boolean areAnagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        int[] freq1 = new int[256];
+        int[] freq2 = new int[256];
+
+        for (int i = 0; i < str1.length(); i++) {
+            freq1[str1.charAt(i)]++;
+            freq2[str2.charAt(i)]++;
+        }
+
+        for (int i = 0; i < 256; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = scanner.nextLine();
+
+        System.out.print("Enter second string: ");
+        String str2 = scanner.nextLine();
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+
+        scanner.close();
+    }
+}
