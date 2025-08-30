1542fb91943ed0762364a7885ad3ec72df19b735
gaurav-11-opp <kuntalgaurav11@gmail.com>
2025-08-30 13:34:06 +0530

    question8.java

---
+++ string_practice/level2/question8.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class VotingEligibility {
+    public static int[] generateAges(int n) {
+        Random rand = new Random();
+        int[] ages = new int[n];
+        for (int i = 0; i < n; i++) {
+            ages[i] = 10 + rand.nextInt(90);
+        }
+        return ages;
+    }
+
+    public static String[][] checkVotingEligibility(int[] ages) {
+        String[][] result = new String[ages.length][2];
+        for (int i = 0; i < ages.length; i++) {
+            result[i][0] = String.valueOf(ages[i]);
+            if (ages[i] < 0) result[i][1] = "false";
+            else if (ages[i] >= 18) result[i][1] = "true";
+            else result[i][1] = "false";
+        }
+        return result;
+    }
+
+    public static void display(String[][] data) {
+        System.out.println("Age\tCan Vote");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i][0] + "\t" + data[i][1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] ages = new int[10];
+        for (int i = 0; i < 10; i++) {
+            ages[i] = sc.nextInt();
+        }
+        String[][] result = checkVotingEligibility(ages);
+        display(result);
+    }
+}
